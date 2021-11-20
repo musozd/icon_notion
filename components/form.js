126055451659c4dcd1773589a4ea8031451472b1
Mustafa Özdemir <mustafa.ozdemir@code.berlin>
2021-11-20 19:22:55 +0100

    Comments

--- components/form.js
+++ components/form.js
@@ -1,14 +1,66 @@
 import { useAuth0 } from '@auth0/auth0-react'
+import { useState, useEffect } from 'react'
+import Comments from './comments'
 
-export default function Form({ onSubmit, text, textSet}) {
-  const { loginWithRedirect, logout, isAuthenticated, user } = useAuth0()
+
+export default function Form() {
+  const {
+    loginWithRedirect,
+    logout,
+    isAuthenticated,
+    user,
+    getAccessTokenSilently
+  } = useAuth0()
+
+  const [text, textSet] = useState('')
+  const [url, urlSet] = useState(null)
+  const [comments, commentsSet] = useState([])
+
+  const fetchComment = async () => {
+    const query = new URLSearchParams({ url })
+    const newUrl = `/api/comment?${query.toString()}`
+    const response = await fetch(newUrl, {
+      method: 'GET'
+    })
+    const data = await response.json()
+    commentsSet(data)
+  }
+
+  useEffect(() => {
+    if (!url) return
+    fetchComment()
+  }, [url])
+
+  useEffect(() => {
+    const url = window.location.origin + window.location.pathname
+    urlSet(url)
+  }, [])
+
+  const onSubmit = async (e) => {
+    e.preventDefault()
+
+    const userToken = await getAccessTokenSilently()
+
+    // text, token, url
+
+    await fetch('/api/comment', {
+      method: 'POST',
+      body: JSON.stringify({ text, userToken, url }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+
+    fetchComment()
+    textSet('')
+  }
 
   return (
-    <form className="mt-10" onSubmit={onSubmit}>
+    <form className="mt-10 m-2" onSubmit={onSubmit}>
       <textarea
         rows="2"
         className="border border-gray-300 rounded w-full block px-2 py-2"
-        onChange={(e) => (e.target.value)}
+        onChange={(e) => textSet(e.target.value)}
         value={text}
       />
 
@@ -20,7 +72,7 @@ export default function Form({ onSubmit, text, textSet}) {
             </button>
             <img src={user.picture} width={30} className="rounded-full" />
             <span>{user.name}</span>
-            <button 
+            <button
               typeof="button"
               onClick={() => logout({ returnTo: process.env.NEXT_PUBLIC_URL })}
             >
@@ -37,6 +89,7 @@ export default function Form({ onSubmit, text, textSet}) {
           </button>
         )}
       </div>
+      <Comments comments={comments} />
     </form>
   )
 }
---
+++ pages/api/comment.js
@@ -0,0 +1,58 @@
+import { nanoid } from 'nanoid'
+import Redis from 'ioredis'
+
+export default async function handler(req, res) {
+  //CREATE
+  if (req.method === 'POST') {
+    const { url, userToken, text } = req.body
+    //add comment to upstash
+
+    if (!url || !userToken || !text)
+      return res.status(400).json({ message: 'missing parameter error' })
+
+    // user verf.
+
+    const userResponse = await fetch(
+      `https://${process.env.NEXT_PUBLIC_AUTH0_AUTH0_DOMAIN}/userinfo`,
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${userToken}`
+        }
+      }
+    )
+    const user = await userResponse.json()
+
+    const comment = {
+      id: nanoid(),
+      createdAt: Date.now(),
+      text,
+      user: {
+        name: user.name,
+        picture: user.picture
+      }
+    }
+
+    // redis connection
+    let redis = new Redis(process.env.REDIS_URL)
+    // redis write
+    redis.lpush(url, JSON.stringify(comment))
+    // redis quit
+    redis.quit()
+    // response
+    res.status(200).json(comment)
+  }
+
+  //FETCH
+  if (req.method === 'GET') {
+    const { url } = req.query
+
+    let redis = new Redis(process.env.REDIS_URL)
+    const comments = await redis.lrange(url, 0, -1)
+    redis.quit()
+
+    const data = comments.map((o) => JSON.parse(o))
+
+    res.status(200).json(data)
+  }
+}
